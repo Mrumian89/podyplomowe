ab09bdbc70e057a3512e69b389f26c8f0232ec6a
mrumian <mrumian@wszib.edu.pl>
2019-11-17 11:34:40 +0100

    next

---
+++ src/Test10.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+
+public class Test10 {
+    public static void main(String[] args) {
+
+        int [] numbers;
+        Point[] points;
+
+        Integer [] series = new Integer [3];
+        series [0] = 10;
+        series [1] = 3;
+        series [2] = 5;
+
+        System.out.println(series[2]); // elementy: 0 1 2
+
+        int [] n = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(n[5]);
+
+        char[] letters = {'a','b','c'};
+
+        System.out.println(letters[letters.length-1]); // odwołanie się do ostatniego elementu
+
+    }
+
+}
